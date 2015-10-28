495577b291839e6717c323171a32299ba6a5badf
John Breen <jjbreen@wpi.edu>
2015-10-28 18:36:18 +0000

    Changed printf to perror

--- src/parse.c
+++ src/parse.c
@@ -402,7 +402,7 @@ int main(int argc, char **argv) {
 
     if (strncmp(argv[1], "--track", 8) == 0) { // TODO some tweaks on tracking
         if (argc < 4) {
-            printf("invalid number of arguments! Exiting ...\n");
+            perror("invalid number of arguments! Exiting ...");
             return 1;
         }
 
